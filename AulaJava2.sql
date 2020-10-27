ca07a324cf841ad4aed70c383a955f9c4f847579
Mateus Maia Agre <mmaiaagr@everis.com>
2020-10-27 16:24:00 -0300

    first

---
+++ AulaJava2.sql
@@ -0,0 +1,26 @@
+CREATE TABLE 
+	Classe(
+		NumeroClasse Int,
+		NomeClasse varchar(100),
+		 PRIMARY KEY (NumeroClasse)
+		);
+
+		SELECT *FROM Classe
+
+		CREATE TABLE 
+	Disciplina(
+		Id Int,
+		Nome varchar(100),
+		 PRIMARY KEY (Id)
+		);
+				SELECT *FROM Disciplina
+
+
+		CREATE TABLE 
+	Professor(
+		Id Int,
+		Nome varchar(100),
+		 PRIMARY KEY (ID)
+		);
+		SELECT *FROM Professor
+
